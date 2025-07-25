24a6b5e89ca17d094003f3923f1576a518958a99
ChanVimean <mrrmeanvlogger@gmail.com>
2025-07-25 14:42:42 +0700

    setup flowbite

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <>
+      <nav>
+        <NavBar />
+      </nav>
+
+      <main></main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
+
+export default App;

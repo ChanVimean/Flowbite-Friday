717080513b2d54ec4d0fb3ae3dd5eebeac198ca2
ChanVimean <mrrmeanvlogger@gmail.com>
2025-08-15 14:57:40 +0700

    add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import PropDrilling1 from "./pages/PropDrilling1";
 import Test from "./pages/Test";
 import Test2 from "./pages/Test2";
 
@@ -12,11 +13,12 @@ const App = () => {
       </nav>
 
       <main>
-        <Home />
+        {/* <Home /> */}
         {/* <Test /> */}
         {/* <Test2 /> */}
+        {/* <PropDrilling1 name={"John"} /> */}
       </main>
- 
+
       <footer>
         <Footer />
       </footer>
---
+++ src/pages/PropDrilling1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropDrilling2 from "./PropDrilling2";
+
+const PropDrilling1 = ({ name }) => {
+  return (
+    <div>
+      <PropDrilling2 name={name} />
+    </div>
+  );
+};
+
+export default PropDrilling1;
